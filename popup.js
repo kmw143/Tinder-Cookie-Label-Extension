88d19cb96f10b643de65f2219b8053ae051a9096
Kami Wang <kmw143@duke.edu>
2025-02-25 23:45:46 +0800

    click flip works

--- popup.js
+++ popup.js
@@ -1,1 +1,9 @@
 console.log("This is a popup!")
+
+document.addEventListener("DOMContentLoaded", function () {
+    document.querySelectorAll(".card").forEach(card => {
+        card.addEventListener("click", function () {
+            this.classList.toggle("flipped");
+        });
+    });
+});
